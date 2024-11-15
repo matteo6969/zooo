7a17371f01feb4ec95575d3c14c663f3773ffe77
alifa27 <ali.fahs@isen.yncrea.fr>
2024-11-15 11:01:41 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 $host = 'localhost'; // Serveur
 $dbname = 'parc_animalier'; // Nom de la base de données
 $username = 'root'; // Nom d'utilisateur MySQL (par défaut sur XAMPP)
-$password = ''; // Mot de passe MySQL (par défaut vide sur XAMPP)
+$password = 'hello'; // Mot de passe MySQL (par défaut vide sur XAMPP)
 
 // Connexion à la base de données
 $db = new mysqli($host, $username, $password, $dbname);
