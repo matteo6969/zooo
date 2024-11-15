16937995a28b5c807bb51a179bf198d79e2d8214
Khalis <khalis.km11@gmail.com>
2024-11-15 10:43:01 +0100

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+// Configuration de la connexion à la base de données
+$host = 'localhost'; // Serveur
+$dbname = 'parc_animalier'; // Nom de la base de données
+$username = 'root'; // Nom d'utilisateur MySQL (par défaut sur XAMPP)
+$password = ''; // Mot de passe MySQL (par défaut vide sur XAMPP)
+
+// Connexion à la base de données
+$db = new mysqli($host, $username, $password, $dbname);
+
+// Vérification de la connexion
+if ($db->connect_error) {
+    die("Erreur de connexion : " . $db->connect_error);
+}
+?>
