15ba09100016ee30234066b5521a4d36258aa6f7
matteo6969 <66360979+matteo6969@users.noreply.github.com>
2024-11-26 14:50:50 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // Configuration de la connexion à la base de données
 $host = 'localhost'; // Serveur
-$dbname = 'parc_animalier'; // Nom de la base de données
+$dbname = 'zoo'; // Nom de la base de données
 $username = 'root'; // Nom d'utilisateur MySQL (par défaut sur XAMPP)
-$password = 'hello'; // Mot de passe MySQL (par défaut vide sur XAMPP)
+$password = ''; // Mot de passe MySQL (par défaut vide sur XAMPP)
 
 // Connexion à la base de données
 $db = new mysqli($host, $username, $password, $dbname);
