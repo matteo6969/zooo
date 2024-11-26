a57ea545353182667c57437e8467061f9e37408c
matteo6969 <66360979+matteo6969@users.noreply.github.com>
2024-11-26 15:29:18 +0100

    Create inscription.php

---
+++ Front-end/inscription.php
@@ -0,0 +1,31 @@
+<?php
+// Informations de connexion à la base
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "zoo";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Échec de la connexion : " . $conn->connect_error);
+}
+
+// Récupérer les données du formulaire
+$nom = htmlspecialchars($_POST['nom']);
+$email = htmlspecialchars($_POST['email']);
+$mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_BCRYPT); // Hachage du mot de passe
+
+// Insérer l'utilisateur dans la base
+$sql = "INSERT INTO utilisateur (nom, email, mot_de_passe) VALUES ('$nom', '$email', '$mot_de_passe')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Inscription réussie. Vous pouvez maintenant vous connecter.";
+    echo "<br><a href='connexion.html'>Se connecter</a>";
+} else {
+    echo "Erreur : " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
